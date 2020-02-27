45d8c721a7f6cf161183c458d2ec54336baea506
gracewahlman <59583145+gracewahlman@users.noreply.github.com>
2020-02-26 16:11:09 -0800

    HTML, CSS, JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 console.log('OK');
+
+// https://cdnjs.cloudflare.com/ajax/libs/plyr/3.5.6/plyr.min.js
+
+const player = new Plyr( '#player', {
+  
+    controls : [ 'play', 'current-time', 'progress' ],
+    
+    keyboard : { focused: true, global: false },
+    
+    seekTime : 30
+  
+  });
+  
+  console.log( player );
