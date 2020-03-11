d4b09990d1dc9c43961969e260791e548d92f913
gracewahlman <59583145+gracewahlman@users.noreply.github.com>
2020-03-10 21:08:40 -0700

    a lot of updates

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,48 @@ const player = new Plyr( '#player', {
 });
 
 console.log( player );
+
+// BACKGROUND borrowed from: https://codepen.io/nashvail/pen/wpGgXO
+
+const colors = ["#fb2e01", "#666547", "#fffeb3", "#79B47C", "#F85F36"];
+
+const numBalls = 70;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
+
+
